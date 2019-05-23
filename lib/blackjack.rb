bd4c7e3bf4bffbdd68727e98223b59f92fa33544
Eliot Howes <ejhowes@gmail.com>
2019-05-23 18:25:00 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -48,10 +48,10 @@ end
 
 def hit?(card_total)
   prompt_user
-  input = get_user_input
-  if input == "h"
+  get_user_input
+  if get_user_input == "h"
     card_total += deal_card
-  elsif input == "s"
+  elsif get_user_input == "s"
     return card_total
   else
     return invalid_command
